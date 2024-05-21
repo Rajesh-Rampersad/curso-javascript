aa8dc2d652d8414624e39ac83914fa5badda434f
rajeshdrg <rajesh@hugtak.com>
2024-05-21 14:37:53 -0300

    seguir clase en  la 84

--- Proyectos/Galeria/rollup.config.mjs
+++ Proyectos/Galeria/rollup.config.mjs
@@ -1,8 +1,18 @@
 // rollup.config.mjs
+
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default {
-	input: 'src/index.js',
-	output: {
-		file: 'public/bundle.js',
-		format: 'cjs'
-	}
+  input: 'src/index.js',
+  output: {
+    file: 'public/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve({
+      extensions: ['.js', '.json']
+    }),
+    commonjs()
+  ]
 };
--- Proyectos/Galeria/src/index.js
+++ Proyectos/Galeria/src/index.js
@@ -1,2 +1,2 @@
-'use strict';
-console.log('Hola mundo!');
+import './cargarCategorias';
+import './eventoCategorias';
